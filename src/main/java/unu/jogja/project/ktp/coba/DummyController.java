9da55351d62b382be2139d55e5c04619b8c38158
isss7 <faisfidi@gmail.com>
2022-05-14 17:32:04 +0700

    update BLOB

---
+++ src/main/java/unu/jogja/project/ktp/coba/DummyController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unu.jogja.project.ktp.coba;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import unu.jogja.project.ktp.Data;
+import unu.jogja.project.ktp.DataJpaController;
+
+/**
+ *
+ * @author ongen
+ */
+@Controller
+public class DummyController {
+    DummyJpaController dummyctrl = new DummyJpaController();
+    List<Dummy> data = new ArrayList<>();
+    @RequestMapping("/dummy")
+//    @ResponseBody
+    public String getDummy(Model model){
+    
+        int record = dummyctrl.getDummyCount();
+        String result = "";
+        try{
+            data = dummyctrl.findDummyEntities().subList(0, record);
+        }
+        catch (Exception e){
+            result=e.getMessage();
+        }
+        
+        model.addAttribute("goDummy", data);
+         model.addAttribute("record", record);
+         
+        return "dummy";
+    }
+    
+    @RequestMapping("/create")
+    public String createDummy(){
+        return "dummy/create";
+    }
+    
+    @PostMapping(value = "/newdata", consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String newDummy(HttpServletRequest data,@RequestParam("gambar") MultipartFile file) throws ParseException, Exception{
+        Dummy dumdata = new Dummy();
+        
+        String id = data.getParameter("id");
+        int iid = Integer.parseInt(id);
+        
+        String tanggal = data.getParameter("date");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(tanggal);
+        
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        byte[] image = file.getBytes();
+        
+        dumdata.setId(iid);
+        dumdata.setTanggal(date);
+        dumdata.setGambar(image);
+        
+        dummyctrl.create(dumdata);
+        
+        return "dummy/create";
+    }
+}
